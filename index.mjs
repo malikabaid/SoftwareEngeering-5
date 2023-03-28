6166a547ee59d8e1a5587c8d538deb5092a033a4
malikabaid <rehmanm1@roehampton.ac.uk>
2023-03-28 16:39:24 +0100

    changes made to index

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import mysql from "mysql2/promise"
+
 
 
 const app = express();
@@ -8,18 +8,13 @@ const port = 3000;
 app.get("/",(req, res) => {
     res.send("Hello and Welcome");
 });
-app.set("view engine", pug);
+
 
 app.use(express.static("static"));
 
 console.log(process.env.NODE_ENV);
 
-const db = await mysql.createConnection({
-    host:process.env.DATABASE_HOST || "localhost",
-    user: "user",
-    password: "password",
-    database: "world",
-})
+
 app.get("/",(req, res) => {
     res.render("index")
 });
