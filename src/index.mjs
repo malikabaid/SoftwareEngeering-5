ed936c0fbb4f3ebc957c3b274834215cc24fa5d6
malikabaid <rehmanm1@roehampton.ac.uk>
2023-03-29 02:16:54 +0100

    amended

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,27 @@
 import express from "express";
-
-
+const express = require("express");
+const mysql = require("mysql2");
 
 const app = express();
 const port = 3000;
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST || "localhost",
+    user: "user",
+    password: "password",
+    database: "world",
+  });
 
 app.get("/",(req, res) => {
     res.send("Hello and Welcome");
 });
 
+app.get("/cities", (req, res) => {
+    db.execute("SELECT * FROM `city`", (err, rows, fields) => {
+      console.log(`/cities: ${rows.length} rows`);
+      return res.send(rows);
+    });
+});
 
 app.use(express.static("static"));
 
