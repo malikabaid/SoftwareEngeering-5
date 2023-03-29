2ed68a67b05dc82144e5e16ee903e08cd9ab27c3
malikabaid <rehmanm1@roehampton.ac.uk>
2023-03-29 22:47:26 +0100

    changes

--- src/index.mjs
+++ src/index.mjs
@@ -1,86 +1,45 @@
-/* Import dependencies */
 import express from "express";
-import mysql from "mysql2/promise";
-import DatabaseService from "./services/database.service.mjs";
+import mysql from "mysql2";
 
-/* Create express instance */
 const app = express();
 const port = 3000;
 
-/* Add form data middleware */
-app.use(express.urlencoded({ extended: true }));
-
-// Integrate Pug with Express
 app.set("view engine", "pug");
 
-// Serve assets from 'static' folder
 app.use(express.static("static"));
 
-const db = await DatabaseService.connect();
-const { conn } = db;
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST || "localhost",
+  user: "user",
+  password: "password",
+  database: "world",
+});
 
-/* Landing route */
+/*app.get("/", (req, res) => {
+  res.send("index");
+})*/
+
+app.get("/cities", (req, res) => {
+  db.execute("SELECT * FROM `city`", (err, rows, fields) => {
+    console.log(`/cities: ${rows.length} rows`);
+    return res.send(rows);
+  });
+});
+
+console.log(process.env.NODE_ENV);
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// Sample API route
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-// Landing route
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-// Gallery route
 app.get("/gallery", (req, res) => {
   res.render("gallery");
 });
 
-// About route
 app.get("/about", (req, res) => {
-  res.render("about", { title: "Boring about page" });
+  res.render("about", {title: "Boring"});
 });
 
-app.get("/cities", async (req, res) => {
-  const [rows, fields] = await db.getCities();
-  /* Render cities.pug with data passed as plain object */
-  return res.render("cities", { rows, fields });
-});
-
-app.get('/cities/:id', async (req, res) => {
-  const cityId = req.params.id;
-  const city = await db.getCity(cityId);
-  return res.render('city', { city });
-})
-
-/* Update a city by ID */
-app.post('/cities/:id', async (req, res) => {
-  const cityId = req.params.id;
-  const { name } = req.body;
-  const sql = `
-    UPDATE city
-    SET Name = '${name}'
-    WHERE ID = '${cityId}';
-  `
-  await conn.execute(sql);
-  return res.redirect(`/cities/${cityId}`);
-})
-
-// Returns JSON array of cities
-app.get("/api/cities", async (req, res) => {
-  const [rows, fields] = await db.getCities();
-  return res.send(rows);
-});
-
-app.get("/api/countries", async (req, res) => {
-  const countries = await db.getCountries();
-  res.send(countries);
-});
-
-// Run server!
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
