e58609afd00aa608e3b512885ab26953e02d11f9
malikabaid <rehmanm1@roehampton.ac.uk>
2023-03-30 05:23:26 +0100

    successful changes

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import express from "express";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 /*const express = require("express");
 const mysql = require("mysql2");*/
@@ -14,7 +14,7 @@ app.use(express.static("static"));
 
 console.log(process.env.NODE_ENV);
 
-const db = mysql.createConnection({
+const db = await mysql.createConnection({
   host: process.env.DATABASE_HOST || "localhost",
   user: "user",
   password: "password",
@@ -29,40 +29,86 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-
 /*app.get("/", (req, res) => {
   res.send("index");
 })*/
-
-/*app.get("/cities", (req, res) => {
+// For Cities
+app.get("/", (req, res) => {
   db.execute("SELECT * FROM `city`", (err, rows, fields) => {
     console.log(`/cities: ${rows.length} rows`);
     return res.send(rows);
   });
-});*/
+});
+// For countries
+app.get("/", (req, res) => {
+  db.execute("SELECT * FROM `country`", (err, rows, fields) => {
+    console.log(`/country: ${rows.length} rows`);
+    return res.send(rows);
+  });
+});
 
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
 app.get("/cities", async (req, res) => {
-  try{
+  try {
     const [rows, fields] = await db.execute("SELECT * FROM `city`");
     return res.render("cities", {rows, fields});
   } catch (err) {
     console.error(err);
   }
   });
+  app.get("/api/cities", async (req, res) => {
+    const [rows, fields] = await db.execute("SELECT * FROM 'city'");
+    return res.send(rows);
+  });
 
-app.get("/gallery", (req, res) => {
-  res.render("gallery");
-});
+  app.get("/countries", async (req, res) => {
+    try {
+      const [rows, fields] = await db.execute("SELECT * FROM `country`");
+      return res.render("countries", {rows, fields});
+    } catch (err) {
+      console.error(err);
+    }
+    });
+    app.get("/api/countries", async (req, res) => {
+      const [rows, fields] = await db.execute("SELECT * FROM 'country'");
+      return res.send(rows);
+    });
 
-app.get("/api/cities", async (req, res) => {
+
+/*app.get("/cities", async (req, res) => {
   const [rows, fields] = await db.execute("SELECT * FROM `city`");
   return res.render(rows);
 
-});
+});*/
 
 app.get("/about", (req, res) => {
   res.render("about", {title: "Boring"});
 });
+
+/*app.get("/cities", async (req, res) => {
+  const [rows, fields] = await db.getCities();
+  return res.render("cities", { rows, fields });
+});*/
+
+/*app.get('/cities/:id', async (req, res) => {
+  const cityId = req.params.id;
+  const city = await db.getCity(cityId);
+  return res.render('city', { city });
+})*/
+
+/*app.get("/api/cities", async (req, res) => {
+  const [rows, fields] = await db.getCities();
+  return res.send(rows);
+});*/
+
+app.get("/api/countries", async (req, res) => {
+  const countries = await db.getCountries();
+  res.send(countries);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
