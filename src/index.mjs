1a4c89e165bce420afb5bee312a3c2bbf56f52a7
malikabaid <rehmanm1@roehampton.ac.uk>
2023-03-30 01:54:16 +0100

    successful changes

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,18 @@
 import express from "express";
 import mysql from "mysql2";
 
+/*const express = require("express");
+const mysql = require("mysql2");*/
+
+//console.log("change");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "pug");
 
 app.use(express.static("static"));
+
+console.log(process.env.NODE_ENV);
 
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST || "localhost",
@@ -15,25 +21,38 @@ const db = mysql.createConnection({
   database: "world",
 });
 
-/*app.get("/", (req, res) => {
-  res.send("index");
-})*/
-
-app.get("/cities", (req, res) => {
-  db.execute("SELECT * FROM `city`", (err, rows, fields) => {
-    console.log(`/cities: ${rows.length} rows`);
-    return res.send(rows);
-  });
-});
-
-console.log(process.env.NODE_ENV);
-
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+/*app.get("/", (req, res) => {
+  res.send("index");
+})*/
+
+/*app.get("/cities", (req, res) => {
+  db.execute("SELECT * FROM `city`", (err, rows, fields) => {
+    console.log(`/cities: ${rows.length} rows`);
+    return res.send(rows);
+  });
+});*/
+
+app.get("/cities", async (req, res) => {
+  try{
+    const [rows, fields] = await db.execute("SELECT * FROM `city`");
+    return res.render("cities", {rows, fields});
+  } catch (err) {
+    console.error(err);
+  }
+  });
+
 app.get("/gallery", (req, res) => {
   res.render("gallery");
+});
+
+app.get("/api/cities", async (req, res) => {
+  const [rows, fields] = await db.execute("SELECT * FROM `city`");
+  return res.render(rows);
+
 });
 
 app.get("/about", (req, res) => {
